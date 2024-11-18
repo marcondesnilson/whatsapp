364c1a813fa6d5e4a4d4ffad443bc1d3fe618f63
Marcondes <marcondesnilson@gmail.com>
2024-11-18 11:49:28 -0300

    adiciona mecanismo de bloqueio ao criar registros de HistoryJobs

--- app/Http/Controllers/util/HistoryJobsUtil.php
+++ app/Http/Controllers/util/HistoryJobsUtil.php
@@ -4,19 +4,26 @@ namespace App\Http\Controllers\util;
 
 use App\Http\Controllers\Controller;
 use App\Models\HistoryJobs;
+use Illuminate\Support\Facades\Cache;
 use Ramsey\Uuid\Uuid;
 
 class HistoryJobsUtil extends Controller
 {
-    public function create($queue,$dados)
+    public function create($queue, $dados)
     {
-        $uuid = Uuid::uuid4();
-        HistoryJobs::insert([
-            'uuid' => $uuid,
-            'dados' => json_encode($dados),
-            'queue' => $queue,
-            'created_at' => now()
-        ]);
-        return $uuid;
+        for ($i = 0; $i < 3; $i++) {
+            $lock = Cache::lock('history_jobs_lock', 10);
+            if ($lock->get()) {
+                $uuid = Uuid::uuid4();
+                HistoryJobs::insert([
+                    'uuid' => $uuid,
+                    'dados' => json_encode($dados),
+                    'queue' => $queue,
+                    'created_at' => now()
+                ]);
+                return $uuid;
+            }
+            sleep(1);
+        }
     }
 }
